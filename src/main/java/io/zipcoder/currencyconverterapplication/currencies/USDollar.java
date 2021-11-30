82957113c7568d654694fc8f75e7e8c37345d5c6
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-11-30 11:41:24 -0500

    Finished Lab

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/USDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/USDollar.java
@@ -1,6 +1,11 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class USDollar implements ConvertableCurrency {
+    @Override
+    public CurrencyType getCurrency() {
+        return CurrencyType.US_DOLLAR;
+    }
 }
